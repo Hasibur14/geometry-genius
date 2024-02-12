713e0e25073274329123a3f8e887513ccd0e6002
hasibur14 <hasib141415@gmail.com>
2024-02-13 00:53:13 +0600

    added triangle calculation

--- scripts/triangle-calculate.js
+++ scripts/triangle-calculate.js
@@ -1,3 +1,21 @@
 function calculateTriangleArea(){
-    console.log('inside function')
+    // triangleBase
+   const triangleBaseInput = document.getElementById('triangle-base');
+   const triangleBaseText = triangleBaseInput.value;
+   const triangleBase = parseFloat(triangleBaseText);
+
+//   triangleHeight 
+const triangleHeightInput = document.getElementById("triangle-height");
+const triangleHeightText = triangleHeightInput.value;
+const triangleHeight = parseFloat(triangleHeightText);
+
+// calculate triangle Area
+const triangleArea = 0.5 * triangleBase * triangleHeight;
+console.log(triangleArea);
+
+
+// display triangle Area
+const  triangleAreaSpan = document.getElementById('triangle-area');
+triangleAreaSpan.innerText = triangleArea;
+   
 }
