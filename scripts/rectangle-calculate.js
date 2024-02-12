61001ce76ef625ed5f5788007e4ff7abb1a76392
hasibur14 <hasib141415@gmail.com>
2024-02-13 01:39:17 +0600

    added rectangle calculation

---
+++ scripts/rectangle-calculate.js
@@ -0,0 +1,22 @@
+function calculateRectangleArea(){
+
+    // rectangle Width :
+    const rectangleWidthInput = document.getElementById('rectangle-width');
+    const rectangleWidthText = rectangleWidthInput.value;
+    const rectangleWidth = parseFloat(rectangleWidthText);
+
+    // Rectangle Length
+    const  rectangleLengthInput = document.getElementById('rectangle-length');
+    const rectangleLengthText = rectangleLengthInput.value;
+    const rectangleLength = parseFloat(rectangleLengthText);
+
+    // calculation area:
+
+    const rectangleArea = rectangleWidth * rectangleLength;
+    console.log(rectangleArea);
+
+    // Display Rectangle Area
+
+    const rectangleSpan = document.getElementById('rectangle-area');
+    rectangleSpan.innerText =  rectangleArea;
+}
