90ce719cd6ffcced7c74fba9f5e9efd1b39105ba
hasibur14 <hasib141415@gmail.com>
2024-02-13 12:02:30 +0600

    added rhombus calculation

---
+++ scripts/rhombus-calculate.js
@@ -0,0 +1,22 @@
+
+function calculateRhombusArea(){
+    const  d1 = getRhombusValue('rhombus-d1');
+    const d2 = getRhombusValue('rhombus-d2');
+
+    const area = 0.5 * d1 * d2;
+    setInnerText('rhombus-area',area);
+ 
+}
+
+function getRhombusValue(value){
+    const inputField = document.getElementById(value);
+    const inputValueText = inputField.value;
+    const inputValue = parseFloat(inputValueText);
+    return inputValue;
+}
+
+
+function setInnerText(elementId,area){
+    const element = document.getElementById(elementId);
+    element.innerText = area;
+}
